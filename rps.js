281c3ef1015537abe1b4bc18739ab6a5a476e51a
manman333231 <198392922+manman333231@users.noreply.github.com>
2025-02-17 09:36:33 -0800

    added ui

--- rps.js
+++ rps.js
@@ -1,6 +1,34 @@
 const choices = ["rock", "paper", "scissors"];
+
 let random = 0, humanChoice = null, computerChoice = null, humanScore = 0, computerScore = 0;
 
+const btn1 = document.querySelector("#btn1");
+const btn2 = document.querySelector("#btn2");
+const btn3 = document.querySelector("#btn3");
+
+const hScore = document.querySelector("#hScore");
+const cScore = document.querySelector("#cScore");
+
+btn1.addEventListener("click", () => {
+  humanChoice = "rock";
+  computerChoice = getComputerChoice();
+  playRound(humanChoice, computerChoice);
+});
+
+btn2.addEventListener("click", () => {
+  humanChoice = "paper";
+  computerChoice = getComputerChoice();
+  playRound(humanChoice, computerChoice);
+});
+
+btn3.addEventListener("click", () => {
+  humanChoice = "scissors";
+  computerChoice = getComputerChoice();
+  playRound(humanChoice, computerChoice);
+});
+
+/* old function without the ui
+ 
 function getHumanChoice() {
 	humanChoice = prompt("rock paper or scissors? ");
 	while (humanChoice === null || !choices.includes(humanChoice.toLowerCase())) {
@@ -12,6 +40,8 @@ function getHumanChoice() {
 
 	return humanChoice;
 }
+
+*/
 
 function getComputerChoice() {
 	random = Math.random();
@@ -26,30 +56,32 @@ function getComputerChoice() {
 
 function playRound(humanchoice, computerchoice) {
 	if (humanchoice == computerchoice) {
-		console.log("Tie!");
+		alert("Tie!");
 	} else if (
 		(humanChoice == "rock" && computerChoice == "scissors") ||
 		(humanChoice == "paper" && computerChoice == "rock") ||
 		(humanChoice == "scissors" && computerChoice == "paper")
 	 ) {
-		console.log("You win! " + humanChoice + " beats " + computerChoice);
+		alert("You win! " + humanChoice + " beats " + computerChoice);
 		humanScore++;
 	} else {
-		console.log("You lose! " + computerChoice + " beats " + humanChoice);
+		alert("You lose! " + computerChoice + " beats " + humanChoice);
 		computerScore++;
 	}
-
-	console.log("Score: You " + humanScore + " Computer " + computerScore);
+  hScore.textContent = humanScore;
+  cScore.textContent = computerScore;
+  if(humanScore == 5) {
+    alert("You are the winner!!!!!!");
+    btn1.removeEventListener("click", e);
+    btn2.removeEventListener("click", e);
+    btn3.removeEventListener("click", e);
+  }
+  if(computerScore == 5) {
+    alert("You are the loser noooo!!!!!!");
+    btn1.removeEventListener("click", e);
+    btn2.removeEventListener("click", e);
+    btn3.removeEventListener("click", e);
+  }
 }
 
-for (let i = 0; i <= 4; i++) {
-	humanChoice = getHumanChoice();
-	computerChoice = getComputerChoice();
-	playRound(humanChoice, computerChoice);
-}
 
-if (humanScore > computerScore ) {
-	console.log("You won the game!");
-} else {
-	console.log("You lost the game!");
-}
